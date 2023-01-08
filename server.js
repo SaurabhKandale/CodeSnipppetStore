ee2a9a0569e37d886f63ce594c29e04b168182f6
Saurabh <saurabhkandale07@gmail.com>
2023-01-08 19:31:30 +0530

    some changes

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ app.get('/api/getdata',async (req,res)=>{
 //     app.use(express.static('FrontEnd/build'));
 // }
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./FrontEnd/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./FrontEnd/build/index.html"));
 });
 
 app.listen(port, () => {
